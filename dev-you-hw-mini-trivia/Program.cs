e37319b41f4a14d5c748942a7a1b9a14d1de64fd
Eli <ezsto92@gmail.com>
2021-05-04 11:41:22 -0400

    refactored code with cumulative scores and while loop

--- dev-you-hw-mini-trivia/Program.cs
+++ dev-you-hw-mini-trivia/Program.cs
@@ -12,19 +12,18 @@ namespace dev_you_hw_mini_trivia
             //Prompt the user to choose between 3 trivia categories of your choice(sports, science, pop culture, etc.)
             //Based on the user’s choice give the user a question that falls in the chosen category
             //Let the user know if their answer is correct or incorrect
-
-
-           
-                Console.WriteLine("Welcome to mini-trivia! Choose a trivia category: Pop culture, Tech, History or select exit to Quit");
-
-                string userAnswer;
-                string triviaChoice = Console.ReadLine().ToLower();
-                int score = 0;
-
+            bool keepGoing = true;
+            int score = 0;
+            string userAnswer;
+            while (keepGoing)
+            {
+                Console.WriteLine("Welcome to mini-trivia! Choose a trivia category: 1) Pop culture, 2) Tech, 3) History or 4) to exit");
+                //string userAnswer;
+                int triviaChoice = int.Parse(Console.ReadLine());
 
                 switch (triviaChoice)
                 {
-                    case "pop culture":
+                    case 1:
                         Console.WriteLine("Who is the orginal DC character that DeadPool was inspired by?");
                         userAnswer = Console.ReadLine().ToLower();
                         if (userAnswer == "deathstroke")
@@ -37,11 +36,11 @@ namespace dev_you_hw_mini_trivia
                         else
                         {
                             Console.WriteLine("Try again");
-                        score--;
-                        Console.WriteLine("Your score: " + score);
+                            score--;
+                            Console.WriteLine("Your score: " + score);
                         }
                         break;
-                    case "tech":
+                    case 2:
                         Console.WriteLine("What car was nicknamed the 'Batmobile'?");
                         userAnswer = Console.ReadLine().ToLower();
                         if (userAnswer == "lincoln futura")
@@ -53,47 +52,66 @@ namespace dev_you_hw_mini_trivia
                         else
                         {
                             Console.WriteLine("Try again");
-                        score--;
-                        Console.WriteLine("Your score: " + score);
-                    }
+                            score--;
+                            Console.WriteLine("Your score: " + score);
+                        }
 
                         break;
-                    case "history":
+                    case 3:
                         Console.WriteLine("In 1816, which US state was admitted to the Union as the 20th state?");
                         userAnswer = Console.ReadLine().ToLower();
 
                         if (userAnswer == "mississippi")
                         {
                             Console.WriteLine("That's correct!");
-                             score++;
+                            score++;
                             Console.WriteLine("Your score: " + score);
                         }
                         else
                         {
                             Console.WriteLine("Try again");
-                        score--;
-                        Console.WriteLine("Your score: " + score);
-                    }
+                            score--;
+                            Console.WriteLine("Your score: " + score);
+                        }
                         break;
-                    case "exit":
+                    case 4:
+                        Console.WriteLine("Goodbye!");
+                        keepGoing = false;
+                        break;
 
 
-                        Console.WriteLine("Thanks for playing!");
-
-                        break;
-                    default:
-                        Console.WriteLine("Select a valid category");
-                        break;
 
                 }
-            
-            
-                Console.WriteLine("Press ENTER to Exit");
-                Console.ReadLine();
-            
-        }
+                 
+                if (triviaChoice <= 0 || triviaChoice >= 5) {
+                    Console.WriteLine("Select a valid number");
+                }
+
+                string keepPlaying;
+
+                Console.WriteLine("Would you like to continue? Y or N?");
+                    keepPlaying = Console.ReadLine().ToLower();
+                if (keepPlaying == "y") { keepGoing = true; }
+                if (keepPlaying == "n") {
+                    Console.WriteLine($"Thank you for playing! Your final score is {score}");
+                    Console.WriteLine("Press ENTER to exit");
+                    Console.ReadLine();
+                    keepGoing = false;
+                }
+
+
+
+
+
+
+            }
+          
+
 
         }
+
+
+    }
     }
 
         
